973ccc80d03a7803f88116e215eef00781ca5a52
albaye <alba.ye99@gmail.com>
2023-10-21 12:32:24 +0100

    Adding a design to a stories file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,9 +4,13 @@ module.exports = {
     '../src/docs/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    'storybook-addon-designs',
+  ],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
   },
-}
+};
--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -8,6 +8,12 @@ export default {
   args: {
     text: 'Comfort food',
   },
+  parameters: {
+    design: {
+      type: 'figspec',
+      url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?type=design&node-id=1906-3469&mode=design&t=gctJq2mbEVLsCjky-4',
+    },
+  },
 } as ComponentMeta<typeof Badge>;
 
 const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
