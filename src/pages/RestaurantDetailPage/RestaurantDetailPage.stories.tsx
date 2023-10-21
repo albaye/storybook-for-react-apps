bc5825361c795aef51282cc008c985a923a87466
albaye <alba.ye99@gmail.com>
2023-10-21 19:32:19 +0100

    App loading scenarios with MSW

--- src/pages/RestaurantDetailPage/RestaurantDetailPage.stories.tsx
+++ src/pages/RestaurantDetailPage/RestaurantDetailPage.stories.tsx
@@ -1,4 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { rest } from 'msw';
+import { BASE_URL } from 'api';
+import { restaurants } from 'stub/restaurants';
 
 import { RestaurantDetailPage } from './RestaurantDetailPage';
 
@@ -26,5 +29,41 @@ Success.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/proto/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?type=design&node-id=169-510&t=pAqBqebU2qk3qLNP-0&scaling=min-zoom&page-id=135%3A257&starting-point-node-id=135%3A258',
+  },
+  msw: {
+    handlers: [rest.get(BASE_URL, (req, res, ctx) => res(ctx.json(restaurants)))],
+  },
+};
+
+export const Loading = Template.bind({});
+Loading.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?type=design&node-id=2152-3158&mode=design&t=pAqBqebU2qk3qLNP-4',
+  },
+  msw: {
+    handlers: [rest.get(BASE_URL, (req, res, ctx) => res(ctx.delay('infinite')))],
+  },
+};
+
+export const NotFound = Template.bind({});
+NotFound.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?type=design&node-id=1097-3785&mode=design&t=pAqBqebU2qk3qLNP-4',
+  },
+  msw: {
+    handlers: [rest.get(BASE_URL, (req, res, ctx) => res(ctx.status(404)))],
+  },
+};
+
+export const Error = Template.bind({});
+Error.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?type=design&node-id=1097-3785&mode=design&t=pAqBqebU2qk3qLNP-4',
+  },
+  msw: {
+    handlers: [rest.get(BASE_URL, (req, res, ctx) => res(ctx.status(500)))],
   },
 };
